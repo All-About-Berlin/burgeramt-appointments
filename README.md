9a5329809b55328e588d7b1623bd38a9dd8c0f5a
Nicolas Bouliane <contact@nicolasbouliane.com>
2022-06-14 15:53:40 +0200

    Changed tool to comply with Berlin.de requirements, improved readme

--- README.md
+++ README.md
@@ -6,11 +6,23 @@ This server looks for Bürgeramt appointment every few seconds, and broadcasts t
 
 ### Standalone
 
-```
-pip install -r requirements.txt
-python3 appointments.py
-```
+1. Set the required environment variables:
+
+    * `BOOKING_TOOL_EMAIL`: Your email. Required [here](https://service.berlin.de/robots.txt).
+    * `BOOKING_TOOL_ID`: A unique identifier for your script. This helps Berlin.de distinguish between different people who run the same script.
+
+2. Run the appointment booking tool
+    ```
+    pip install -r requirements.txt
+    python3 appointments.py
+    ```
+
+The tool outputs the appointments it finds and the errors to the console, and broadcasts them with websockets.
 
 ### Docker
 
 A Dockerfile is supplied with the repository.
+
+## Notes
+
+The polling rate is limited to 180 seconds (3 minutes), as required by the Berlin.de IKT-ZMS team (ikt-zms@seninnds.berlin.de).
--- appointments.py
+++ appointments.py
@@ -6,6 +6,7 @@ import asyncio
 import csv
 import json
 import logging
+import os
 import pytz
 import random
 import requests
@@ -21,17 +22,19 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-common_user_agents = (
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-)
+try:
+    # Berlin.de requires the user agent to include your email
+    email = os.environ['BOOKING_TOOL_EMAIL']
+
+    # This allows Berlin.de to distinguish different people running the same tool
+    script_id = os.environ['BOOKING_TOOL_ID']
+except KeyError:
+    logger.exception("You must set the BOOKING_TOOL_EMAIL and BOOKING_TOOL_ID environment variables.")
+
 
 timezone = pytz.timezone('Europe/Berlin')
 appointments_url = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,122243,327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,327539,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122301,327282,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,122281,327352,122279,329772,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F'
-delay = 30
+delay = 180  # Minimum allowed by Berlin.de's IKT-ZMS team
 
 
 def datetime_to_json(datetime_obj):
@@ -56,16 +59,22 @@ def get_appointments():
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': random.choice(common_user_agents),
+        'User-Agent': f"Mozilla/5.0 AppointmentBookingTool/1.1 (https://github.com/nicbou/burgeramt-appointments-websockets; {email}; {script_id})",
         'Accept-Language': 'en-gb',
         'Accept-Encoding': 'gzip, deflate',
         'Connection': 'keep-alive',
     }
 
+    # Load the first two months
     response_p1 = session.get(appointments_url, headers=headers)
     response_p1.raise_for_status()
     time.sleep(5)
-    response_p2 = session.get(f'https://service.berlin.de/terminvereinbarung/termin/day/{next_month_timestamp}/', headers=headers)
+
+    # Load the next two months
+    response_p2 = session.get(
+        f'https://service.berlin.de/terminvereinbarung/termin/day/{next_month_timestamp}/',
+        headers=headers
+    )
     response_p2.raise_for_status()
 
     return sorted(parse_appointment_dates(response_p1.text) + parse_appointment_dates(response_p2.text))
@@ -86,7 +95,7 @@ def look_for_appointments():
     global delay
     try:
         appointments = get_appointments()
-        delay = 30
+        delay = 180
         return {
             'time': datetime_to_json(datetime.now()),
             'status': 200,
@@ -95,8 +104,8 @@ def look_for_appointments():
             'connectedClients': len(connected_clients),
         }
     except requests.HTTPError as err:
-        logger.warning(f"Got {err.response.status_code} error. Checking in 300 seconds")
-        delay = 300
+        delay = 360
+        logger.warning(f"Got {err.response.status_code} error. Checking in {delay} seconds")
         return {
             'time': datetime_to_json(datetime.now()),
             'status': 502,
