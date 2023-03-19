da91a613df85453d7f7b270ad7ff916902c2d40f
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-03-19 13:55:26 +0100

    Handle connection errors

--- appointments.py
+++ appointments.py
@@ -21,10 +21,10 @@ logger = logging.getLogger(__name__)
 logging.getLogger('websockets.server').setLevel(logging.ERROR)
 
 try:
-    # Berlin.de requires the user agent to include your email
+    # Berlin.de requires the user agent to include your email.
     email = os.environ['BOOKING_TOOL_EMAIL']
 
-    # This allows Berlin.de to distinguish different people running the same tool
+    # This allows Berlin.de to distinguish different between people running the same tool.
     script_id = os.environ['BOOKING_TOOL_ID']
 except KeyError:
     logger.exception("You must set the BOOKING_TOOL_EMAIL and BOOKING_TOOL_ID environment variables.")
@@ -32,7 +32,7 @@ except KeyError:
 
 timezone = pytz.timezone('Europe/Berlin')
 appointments_url = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,122243,327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,327539,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122301,327282,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,122281,327352,122279,329772,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F'
-delay = 180  # Minimum allowed by Berlin.de's IKT-ZMS team
+delay = 180  # Minimum allowed by Berlin.de's IKT-ZMS team.
 
 
 def datetime_to_json(datetime_obj):
@@ -109,6 +109,15 @@ def look_for_appointments():
             'time': datetime_to_json(datetime.now()),
             'status': 502,
             'message': f'Could not fetch results from Berlin.de - Got HTTP {err.response.status_code}.',
+            'appointmentDates': [],
+            'connectedClients': len(connected_clients),
+        }
+    except requests.exceptions.ConnectionError:
+        logger.warning("Could not connect to Berlin.de.")
+        return {
+            'time': datetime_to_json(datetime.now()),
+            'status': 502,
+            'message': 'Could not fetch results from Berlin.de - Got connection error.',
             'appointmentDates': [],
             'connectedClients': len(connected_clients),
         }
