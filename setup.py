85f93e6b5de99b51515ad000fecfd28f061a03b8
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-04-24 11:23:12 +0200

    Update version to 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='berlin-appointment-finder',
-    version='1.0.2',
+    version='1.0.3',
     description='Finds appointments at the Berlin Bürgeramt, broadcasts them via websockets',
     long_description=long_description,
     long_description_content_type='text/markdown',
