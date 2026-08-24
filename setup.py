6911c2940557a9ab067ad261391421566d9d830b
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-08-24 15:53:40 +0200

    Create new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='berlin-appointment-finder',
-    version='2.0.0',
+    version='2.0.1',
     description='Finds appointments at the Berlin Bürgeramt, broadcasts them via websockets',
     long_description=long_description,
     long_description_content_type='text/markdown',
